9820583750b34e2d3569f85b333d8f04da0846b3
anamar19 <ana340147@gmail.com>
2023-05-22 11:51:06 +0300

    Java-automation-register-test

---
+++ test-automation-register-form/java-automation/src/test/java/dataProvider/SignUpPageDataProvider.java
@@ -0,0 +1,12 @@
+package dataProviders;
+
+import org.testng.annotations.DataProvider;
+
+public class SignUpPageDataProvider {
+    @DataProvider(name = "signUpPageDataProvider")
+    public Object[][] SignUpPageDataProvider() {
+        return new Object[][] {
+                {"chrome", true},
+        };
+    }
+}
